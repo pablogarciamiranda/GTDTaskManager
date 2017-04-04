9ea5470a748d48c874451272a9b01257a9b27850
pablogarciamiranda <uo245530@uniovi.es>
2017-04-04 17:06:33 +0200

    Missing inheritance from one RemoteService

--- sdi3-5EJB/ejbModule/uo/sdi/business/impl/admin/RemoteAdminService.java
+++ sdi3-5EJB/ejbModule/uo/sdi/business/impl/admin/RemoteAdminService.java
@@ -2,7 +2,9 @@ package uo.sdi.business.impl.admin;
 
 import javax.ejb.Remote;
 
+import uo.sdi.business.AdminService;
+
 @Remote
-public interface RemoteAdminService {
+public interface RemoteAdminService extends AdminService {
 
 }
